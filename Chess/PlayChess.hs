f309b2490396aafba4a51f7c6a01d77f617839ec
tvatsal1996 <tvatsal1996@gmail.com>
2018-03-22 15:07:50 +0530

    modified PlayChess.hs

--- Chess/PlayChess.hs
+++ Chess/PlayChess.hs
@@ -11,26 +11,66 @@ import Game
 import Rank
 import ComputerPlayer
 
-play game = do
-    putStr "Enter source column : "
-    input1 <- getLine
-    let cfrom = (read input1 :: Int)
-    putStr "Enter source row : "
-    input2 <- getLine
-    let rfrom = (read input2 :: Int)
-    putStr "Enter destination column : "
-    input3 <- getLine
-    let cto = (read input3 :: Int)
-    putStr "Enter destination row : "
-    input4 <- getLine
-    let rto = (read input4 :: Int)
-    let g = move (Field cfrom rfrom) (Field cto rto) Nothing game
-    putStrLn "------------------------------------------------------------------------------------------------------------------------------------------------------\n"
-    printBoard (gameBoard (fromJust g))
-    putStrLn "------------------------------------------------------------------------------------------------------------------------------------------------------\n"
-    let botmove = makeMove (fromJust g)
-    printBoard (gameBoard (fromJust botmove))
-    putStrLn "------------------------------------------------------------------------------------------------------------------------------------------------------\n"
-    play (fromJust botmove)
+isequal :: Char -> Bool
+isequal ch | ch == ' ' = True
+           | otherwise = False 
 
-playChess = play GameStart
+isnotequal :: Char -> Bool
+isnotequal ch | ch == ' ' = False
+              | otherwise = True
+
+split :: [Char] -> ([Char], [Char])
+split input = do
+    let new = dropWhile isequal input
+    let input1 = takeWhile isnotequal new
+    let new1 = dropWhile isnotequal new
+    let new2 = dropWhile isequal new1
+    let input2 = takeWhile isnotequal new2
+    let x = (input1,input2)
+    fromJust (return x)
+
+getFigure :: [Char] -> Maybe Figure
+getFigure str | fst figure == "Rook" && snd figure == "White"   = Just (Figure Rook White)
+              | fst figure == "Knight" && snd figure == "White" = Just (Figure Knight White)
+              | fst figure == "Bishop" && snd figure == "White" = Just (Figure Bishop White)
+              | fst figure == "Queen" && snd figure == "White"  = Just (Figure Queen White)
+              | fst figure == "King" && snd figure == "White"   = Just (Figure King White)
+              | fst figure == "Pawn" && snd figure == "White"   = Just (Figure Pawn White)
+              | fst figure == "Rook" && snd figure == "Black"   = Just (Figure Rook Black)
+              | fst figure == "Knight" && snd figure == "Black" = Just (Figure Knight Black)
+              | fst figure == "Bishop" && snd figure == "Black" = Just (Figure Bishop Black)
+              | fst figure == "Queen" && snd figure == "Black"  = Just (Figure Queen Black)
+              | fst figure == "King" && snd figure == "Black"   = Just (Figure King Black)
+              | fst figure == "Pawn" && snd figure == "Black"   = Just (Figure Pawn Black)
+              | otherwise                                       = Nothing
+              where figure = split str
+
+play1 game = do
+    putStr "Enter source column and row : "
+    input1 <- getLine
+    let source = split input1
+    let cfrom = (read (fst source) :: Int)
+    let rfrom = (read (snd source) :: Int)
+    putStr "Enter destination column and row : "
+    input2 <- getLine
+    let destination = split input2
+    let cto = (read (fst destination) :: Int)
+    let rto = (read (snd destination) :: Int)
+    putStr "Enter figure and color of the promotional move if any : "
+    input3 <- getLine
+    let figure = getFigure input3
+    let g = move (Field cfrom rfrom) (Field cto rto) figure game
+    putStrLn "------------------------------------------------------------------------------------------------------------------------------------------------------\n"
+    case g of
+        Nothing    -> do
+            putStrLn "Invalid move, please try another move"
+            play1 game
+        Just ngame -> do
+            printBoard (gameBoard ngame)
+            putStrLn "------------------------------------------------------------------------------------------------------------------------------------------------------\n"
+            let botmove = makeMove ngame
+            printBoard (gameBoard (fromJust botmove))
+            putStrLn "------------------------------------------------------------------------------------------------------------------------------------------------------\n"
+            play1 (fromJust botmove)
+
+playChess = play1 GameStart
